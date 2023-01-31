06a2dac10a4d5c933769df815e6ad2be8fbeb407
Jacob Studstill <jacob.studstill@gmail.com>
2023-01-31 16:12:21 -0500

    Page is now displaying the read notes so I am pushing

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const db = require('../db/db.json')
 const fs = require('fs')
 
-
+//Get for the router
 router.get('/notes', (req, res) => {
   fs.readFile('db/db.json','utf-8', (err,data) => {
     return err ? console.log(err) : res.json(JSON.parse(data))})
